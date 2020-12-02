d61fe31019a4464b4b9d085ff473cbc80bffc22d
DryRun <david.renhwa.yu@gmail.com>
2020-12-02 12:02:10 -0600

    Fix notebook launcher

--- launchnb.sh
+++ launchnb.sh
@@ -4,6 +4,6 @@ if [ -z "$1" ] ; then
 else
     PORT=$1
 fi
-jupyter notebook --no-browser --port=$PORT
 echo "On your local machine, do:"
 echo "ssh -N -L localhost:$PORT:localhost:$PORT $(whoami)@brux7.hep.brown.edu"
+jupyter notebook --no-browser --port=$PORT
