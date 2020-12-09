1c10f728d6315750feb599e37729e557f01109cf
David Yu <david_yu@brown.edu>
2020-12-09 11:37:01 -0500

    Update setup scripts

--- firsttime.sh
+++ firsttime.sh
@@ -2,9 +2,9 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p miniconda/
 source miniconda/bin/activate
-conda create -n toffea python=3.6
+conda create -n toffea python=3.7
 conda activate toffea
-pip install coffea
+pip install coffea==0.6.47
 conda install -c conda-forge xrootd root
 
 pip install --user --editable .
