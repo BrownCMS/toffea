e6c3c07242c86e4d14a18b4c58ad757334a7a2e8
DryRun <david.renhwa.yu@gmail.com>
2020-12-14 23:57:04 -0600

    Skeleton analyzer

--- toffea/histograms.py
+++ toffea/histograms.py
@@ -20,120 +20,191 @@ from coffea import hist
 from coffea import lookup_tools
 from coffea import util
 import coffea.processor as processor
-import awkward
+import awkward1 as awk
 import copy
 from coffea.analysis_objects import JaggedCandidateArray
 from toffea.common.binning import dijet_binning
 
 np.set_printoptions(threshold=np.inf)
 
-class DataProcessor(processor.ProcessorABC):
-  def __init__(self):
-    # Histograms
-    dataset_axis = hist.Cat("dataset", "Primary dataset")
-    selection_axis = hist.Cat("selection", "Selection name")
+class HackSchema(NanoAODSchema):
+    def __init__(self, base_form):
+        base_form["contents"].pop("Muon_fsrPhotonIdx", None)
+        for key in list(base_form["contents"]):
+            if "_genPartIdx" in key:
+                base_form["contents"].pop(key, None)
+        super().__init__(base_form)
 
-    self._accumulator = processor.dict_accumulator()
-    self._accumulator["nevents"] = processor.defaultdict_accumulator(int)
+class TrijetHistogramMaker(processor.ProcessorABC):
+    def __init__(self):
+        # Histograms
+        dataset_axis = hist.Cat("dataset", "Primary dataset")
+        selection_axis = hist.Cat("selection", "Selection name")
 
-    self._accumulator["mjjj"] = hist.Hist("Events", 
-                                          dataset_axis, 
-                                          selection_axis, 
-                                          hist.Bin("mjjj", r"$m_{jjj}$ [GeV]", dijet_binning),
-                                        )
+        self._accumulator = processor.dict_accumulator()
+        self._accumulator["total_events"] = processor.defaultdict_accumulator(int)
+
+        # Define histograms here
+        self._accumulator["mjjj"] = hist.Hist("Events", 
+                                                dataset_axis, 
+                                                selection_axis, 
+                                                hist.Bin("mjjj", r"$m_{jjj}$ [GeV]", dijet_binning), 
+                                                )
+
+        for pair in [(0, 1), (1, 2), (2, 0)]:
+            self._accumulator[f"m{pair[0]}{pair[1]}"] = hist.Hist("Events", 
+                                                    dataset_axis, 
+                                                    selection_axis, 
+                                                    hist.Bin(f"m{pair[0]}{pair[1]}", f"$m_{{{pair[0]}{pair[1]}}}$ [GeV]$", dijet_binning))
+            self._accumulator[f"dR{pair[0]}{pair[1]}"] = hist.Hist("Events", 
+                                                    dataset_axis, 
+                                                    selection_axis, 
+                                                    hist.Bin(f"dR{pair[0]}{pair[1]}", f"$\\Delta R_{{{pair[0]}{pair[1]}}}$ [GeV]$", 75, 0., 7.5))
+            self._accumulator[f"dEta{pair[0]}{pair[1]}"] = hist.Hist("Events", 
+                                                    dataset_axis, 
+                                                    selection_axis, 
+                                                    hist.Bin(f"dEta{pair[0]}{pair[1]}", f"$\\Delta \\eta_{{{pair[0]}{pair[1]}}}$ [GeV]$", 75, 0., 7.5))
 
 
-  @property
-  def accumulator(self):
-    return self._accumulator
+    @property
+    def accumulator(self):
+        return self._accumulator
 
-  def process(self, df):
-    output = self._accumulator.identity()
-    dataset_name = df['dataset']
-    match_subjob = self._re_subjob.search(dataset_name)
-    if match_subjob:
-      dataset_name.replace(match_subjob.group("subjob_tag"), "")
-    output["nevents"][dataset_name] += df.size
+    def process(self, events):
+        output = self._accumulator.identity()
+        dataset_name = events['dataset']
+        output["total_events"][dataset_name] += events.size
 
+        # Require 3 jets
+        #events_3j = events[events.nJet >= 3]
+        jet_mask = (events.Jet.pt > 30.) & (abs(events.Jet.eta) < 2.5) & (events.Jet.isTight)
+        event_mask = awk.sum(jet_mask, axis=1) >= 3
+        events_3j = events[event_mask]
 
-    return output
+        # Reduce jet mask to only events with 3 good jets
+        jet_mask = jet_mask[event_mask]
 
-  def postprocess(self, accumulator):
-      return accumulator
+        # Array of the jets to consider for trijet resonance
+        selected_jets = events_3j.Jet[jet_mask][:, :3]
+
+        # Pairs of jets
+        #pairs = awk.argcombinations(selected_jets, 2)
+        #jet_i, jet_j = awk.unzip(pairs)
+        pairs = [(0, 1), (1, 2), (2, 0)]
+        jet_i, jet_j = zip(*pairs) # Returns [0, 1, 2] , [1, 2, 0]
+
+        m_ij = (events_3j.Jet[:, jet_i] + events_3j.Jet[:, jet_j]).mass
+        dR_ij = events_3j.Jet[:, jet_i].delta_r(events_3j.Jet[:, jet_j])
+        dEta_ij = abs(events_3j.Jet[:, jet_i].eta - events_3j.Jet[:, jet_j].eta)
+
+        max_dR   = awk.max(dR_ij, axis=1)
+        max_dEta = awk.max(dEta_ij, axis=1)
+        min_dR   = awk.min(dR_ij, axis=1)
+        min_dEta = awk.min(dEta_ij, axis=1)
+        min_pT = awk.min()
+
+        #m01 = (selected_jets[:, 0] + selected_jets[:, 1]).mass
+        #m12 = (selected_jets[:, 1] + selected_jets[:, 2]).mass
+        #m20 = (selected_jets[:, 2] + selected_jets[:, 0]).mass
+        #dR01 = (selected_jets[:, 0].delta_r(selected_jets[:, 1]))
+        #dR12 = (selected_jets[:, 1].delta_r(selected_jets[:, 2]))
+        #dR20 = (selected_jets[:, 2].delta_r(selected_jets[:, 0]))
+        #dEta01 = abs(selected_jets[:, 0].eta - selected_jets[:, 1].eta)
+        #dEta12 = abs(selected_jets[:, 1].eta - selected_jets[:, 2].eta)
+        #dEta20 = abs(selected_jets[:, 2].eta - selected_jets[:, 0].eta)
+
+        m3j = selected_jets.sum().mass #(selected_jets[:, 0] + selected_jets[:, 1] + selected_jets[:, 2]).mass
+
+        # Event selection
+        selections = {}
+        selections["sr"] = PackedSelection()
+        selections["sr"].add("MaxDEta", max_dEta < 1.3)
+        selections["sr"].add("MinDR", min_dR > 0.4)
+        selections["sr"].add("MinJetPt", awk.min(selected_jets.pt) > 50.)
+         
+
+        return output
+
+    def postprocess(self, accumulator):
+            return accumulator
 
 if __name__ == "__main__":
 
-  import argparse
-  parser = argparse.ArgumentParser(description="Make histograms for B FFR data")
-  parser.add_argument("--datasets", "-d", type=str, help="List of datasets to run (comma-separated")
-  parser.add_argument("--workers", "-w", type=int, default=16, help="Number of workers")
-  parser.add_argument("--quicktest", "-q", action="store_true", help="Run a small test job")
-  parser.add_argument("--save_tag", "-s", type=str, help="Save tag for output file")
-  #parser.add_argument("--nopbar", action="store_true", help="Disable progress bar (do this on condor)")
-  parser.add_argument("--condor", action="store_true", help="Flag for running on condor")
-  args = parser.parse_args()
-
-  if args.quicktest:
-    datasets = ["Run2018D_part2_subjob0"]
-  else:
-    datasets = args.datasets.split(",")
-
-  from data_index import in_txt
-
-  dataset_files = {}
-  for dataset_name in datasets:
-    if not dataset_name in in_txt:
-      raise ValueError(f"Dataset {dataset_name} not in dictionary.")
-
-    with open(in_txt[dataset_name], 'r') as filelist:
-      dataset_files[dataset_name] = [x.strip() for x in filelist.readlines()]
+    import argparse
+    parser = argparse.ArgumentParser(description="Make histograms for B FFR data")
+    parser.add_mutually_exclusive_group("input_group")
+    input_group.add_argument("--subsamples", "-d", type=str, help="List of subsamples to run (comma-separated")
+    input_group.add_argument("--quicktest", "-q", action="store_true", help="Run a small test job")
+    parser.add_argument("--workers", "-w", type=int, default=16, help="Number of workers")
+    parser.add_argument("--save_tag", "-s", type=str, help="Save tag for output file")
+    #parser.add_argument("--nopbar", action="store_true", help="Disable progress bar (do this on condor)")
+    parser.add_argument("--condor", action="store_true", help="Flag for running on condor")
+    args = parser.parse_args()
 
     if args.quicktest:
-      dataset_files[dataset_name] = dataset_files[dataset_name][:3]
+        subsamples = ["Run2018D_part2_subjob0"]
+    else:
+        subsamples = args.subsamples.split(",")
+
+    from data_index import in_txt
+
+    subsample_files = {}
+    for subsample_name in subsamples:
+        if not subsample_name in in_txt:
+            raise ValueError(f"Dataset {subsample_name} not in dictionary.")
+
+        with open(in_txt[subsample_name], 'r') as filelist:
+            subsample_files[subsample_name] = [x.strip() for x in filelist.readlines()]
+
+        if args.quicktest:
+            subsample_files[subsample_name] = subsample_files[subsample_name][:3]
+
+        if args.condor:
+            # Copy input files to worker node... seems to fail sporadically when reading remote input files :(
+            local_filelist = []
+            for remote_file in subsample_files[subsample_name]:
+                retry_counter = 0
+                expected_path = f"{os.path.expandvars('$_CONDOR_SCRATCH_DIR')}/{os.path.basename(remote_file)}"
+                while retry_counter < 5 and not (os.path.isfile(expected_path) and os.path.getsize(expected_path) > 1.e6):
+                    if retry_counter >= 1:
+                        time.sleep(10)
+                    os.system(f"cp {remote_file} $_CONDOR_SCRATCH_DIR")
+                    retry_counter += 1
+                if not (os.path.isfile(expected_path) and os.path.getsize(expected_path) > 1.e6):
+                    raise RuntimeError("FATAL : Failed to copy file {}".format(remote_file))
+                os.system("ls -lrth $_CONDOR_SCRATCH_DIR")
+                local_filelist.append(f"{os.path.expandvars('$_CONDOR_SCRATCH_DIR')}/{os.path.basename(remote_file)}")
+            subsample_files[subsample_name] = local_filelist
+
+    ts_start = time.time()
+    print(subsample_files)
+
+
+    output = processor.run_uproot_job(subsample_files,
+                                        treename='Events',
+                                        processor_instance=TrijetHistogramMaker(),
+                                        executor=processor.futures_executor,
+                                        executor_args={'workers': args.workers, 'flatten': False, 'status':not args.condor},
+                                        chunksize=50000,
+                                        {"schema": HackSchema}
+                                        # maxchunks=1,
+                                        )
+    util.save(output, f"DataHistograms_{args.save_tag}.coffea")
+
+    # Performance benchmarking and cutflows
+    ts_end = time.time()
+    total_events = 0
+    subsample_nevents = {}
+    for k, v in output['nevents'].items():
+        if k in subsample_nevents:
+            subsample_nevents[k] += v
+        else:
+            subsample_nevents[k] = v
+        total_events += v
+
+    print("Total time: {} seconds".format(ts_end - ts_start))
+    print("Total rate: {} Hz".format(total_events / (ts_end - ts_start)))
 
     if args.condor:
-      # Copy input files to worker node... seems to fail sporadically when reading remote :(
-      local_filelist = []
-      for remote_file in dataset_files[dataset_name]:
-        retry_counter = 0
-        expected_path = f"{os.path.expandvars('$_CONDOR_SCRATCH_DIR')}/{os.path.basename(remote_file)}"
-        while retry_counter < 5 and not (os.path.isfile(expected_path) and os.path.getsize(expected_path) > 1.e6):
-          if retry_counter >= 1:
-            time.sleep(10)
-          os.system(f"cp {remote_file} $_CONDOR_SCRATCH_DIR")
-          retry_counter += 1
-        if not (os.path.isfile(expected_path) and os.path.getsize(expected_path) > 1.e6):
-          raise RuntimeError("FATAL : Failed to copy file {}".format(remote_file))
-        os.system("ls -lrth $_CONDOR_SCRATCH_DIR")
-        local_filelist.append(f"{os.path.expandvars('$_CONDOR_SCRATCH_DIR')}/{os.path.basename(remote_file)}")
-      dataset_files[dataset_name] = local_filelist
-
-  ts_start = time.time()
-  print(dataset_files)
-  output = processor.run_uproot_job(dataset_files,
-                                treename='Events',
-                                processor_instance=DataProcessor(),
-                                executor=processor.futures_executor,
-                                executor_args={'workers': args.workers, 'flatten': False, 'status':not args.condor},
-                                chunksize=50000,
-                                # maxchunks=1,
-                            )
-  util.save(output, f"DataHistograms_{args.save_tag}.coffea")
-
-  # Performance benchmarking and cutflows
-  ts_end = time.time()
-  total_events = 0
-  dataset_nevents = {}
-  for k, v in output['nevents'].items():
-    if k in dataset_nevents:
-      dataset_nevents[k] += v
-    else:
-      dataset_nevents[k] = v
-    total_events += v
-
-  print("Total time: {} seconds".format(ts_end - ts_start))
-  print("Total rate: {} Hz".format(total_events / (ts_end - ts_start)))
-
-  if args.condor:
-    os.system("mkdir -pv $_CONDOR_SCRATCH_DIR/hide")
-    os.system("mv $_CONDOR_SCRATCH_DIR/*Run2018*root $_CONDOR_SCRATCH_DIR/hide")
+        os.system("mkdir -pv $_CONDOR_SCRATCH_DIR/hide")
+        os.system("mv $_CONDOR_SCRATCH_DIR/*Run2018*root $_CONDOR_SCRATCH_DIR/hide")
